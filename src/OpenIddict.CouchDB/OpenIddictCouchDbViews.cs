bb64a3ac2d7f9d8e9aa8d26fe7baec908c9c868c
Panos Athanasiou <panos_ath@outlook.com>
2021-02-22 19:19:20 +0200

    Add OpenIddictCouchDbViews

---
+++ src/OpenIddict.CouchDB/OpenIddictCouchDbViews.cs
@@ -0,0 +1,47 @@
+﻿#pragma warning disable CA1034 // Nested types should not be visible
+
+namespace OpenIddict.CouchDB
+{
+    public static class OpenIddictCouchDbViews
+    {
+        public static class Application
+        {
+            public static (string design, string view) All { get; set; } = ("application", "all");
+
+            public static (string design, string view) Count { get; set; } = ("application", "count");
+        }
+
+        public static class Authorization
+        {
+            public static (string design, string view) All { get; set; } = ("authorization", "all");
+
+            public static (string design, string view) Count { get; set; } = ("authorization", "count");
+
+            public static (string design, string view) ApplicationId { get; set; } = ("authorization", "application_id");
+
+            public static (string design, string view) Subject { get; set; } = ("authorization", "subject");
+        }
+
+        public static class Scope
+        {
+            public static (string design, string view) All { get; set; } = ("scope", "all");
+
+            public static (string design, string view) Count { get; set; } = ("scope", "count");
+
+            public static (string design, string view) Name { get; set; } = ("scope", "name");
+        }
+
+        public static class Token
+        {
+            public static (string design, string view) All { get; set; } = ("token", "all");
+
+            public static (string design, string view) Count { get; set; } = ("token", "count");
+
+            public static (string design, string view) Prune { get; set; } = ("token", "prune");
+
+            public static (string design, string view) ApplicationId { get; set; } = ("token", "application_id");
+
+            public static (string design, string view) AuthorizationId { get; set; } = ("token", "authorization_id");
+        }
+    }
+}
